6bc2d95f145108b363742905cfcf294fbb71a93a
coconutmilk0125 <1enke@sina.cn>
2022-01-09 23:48:24 +0100

    first

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class MyHashMap {
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private int initialSize = 16;
+
+    private double loadFactor = 0.75;
+
+    public MyHashMap(int initialSize, double loadFactor) {
+        this.initialSize = initialSize;
+        this.loadFactor = loadFactor;
+    }
+
+    public MyHashMap(int initialSize) {
+        this.initialSize = initialSize;
+    }
+
+    public MyHashMap() {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
 }
